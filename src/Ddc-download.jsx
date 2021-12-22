7315e6aac71a77d75b6e93c9b42315e315f0f77c
Prasanna Kondapaneni <prasanna@teamiconix.com>
2021-12-22 06:14:48 -0800

    playing with material UI styling

--- src/Ddc-download.jsx
+++ src/Ddc-download.jsx
@@ -1,4 +1,12 @@
 import { useState } from "react";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 import { downloadFromDDC } from "./utils/download";
 
 const DdcDownload = (props) => {
@@ -12,11 +20,26 @@ const DdcDownload = (props) => {
 
 	return (
 		<div className="Minter">
-			<h2> DDC Download </h2>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        style={{ backgroundColor: "black"}}
+        alt="Download"
+        height="140"
+        image="https://cere.network/assets/images/home/2nd.svg"
+      />
+      <CardContent>
+			<h2> Download from DDC </h2>
 	        <span> ContentID to download from DDC: </span>
 	        <input placeholder="Enter CID to decrypt" value={cid} onChange={onCidInput}/>
-			<button onClick={initiateDownload}> DDC Download </button>
+      </CardContent>
+      <CardActions>
+			<Button onClick={initiateDownload}> DDC Download </Button>
 			{ content ? (<div>Content retrieved is: {content} </div>) : null}
+        <Button size="small">Learn More</Button>
+        {/* <Button size="small">&lt;/&gt;</Button> */}
+      </CardActions>
+    </Card>
 		</div>
 	);
 };
--- src/Ddc-upload.jsx
+++ src/Ddc-upload.jsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
 import { upload2DDC } from "./utils/upload";
+import Button from '@mui/material/Button';
+
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 
 const DdcUpload =  (_) => {
 	const datestr = new Date().toLocaleTimeString();
@@ -15,15 +24,39 @@ const DdcUpload =  (_) => {
     const onConentInput = e => setContent(e.target.value);
 	return (
 		<div className="Minter">
-			<h2> DDC Upload </h2>
-	        <span> Content to upload to DDC: </span>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        style={{ backgroundColor: "black"}}
+        alt="Upload"
+        height="140"
+        image="https://cere.network/assets/images/home/graphic-freeport.svg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Upload to DDC
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Intro text about DDC & how easy it is to upload etc.
+        </Typography>
+
+        <Typography variant="body3" color="text.secondary">
+        	        <span> Content to upload to DDC: </span>
 	        <input placeholder="Enter data to save" value={content} onChange={onConentInput}/>
-			<button onClick={initiateUpload}> DDC Upload </button>
 			{ cid ? (<div>Saved. DDC content id: {cid}
 					</div>) : null}
+        </Typography>
 
+      </CardContent>
+      <CardActions>
+        <Button onClick={initiateUpload} size="small">Upload</Button>
+        <Button size="small">Learn More</Button>
+        {/* <Button size="small">&lt;/&gt;</Button> */}
+      </CardActions>
+    </Card>
 		</div>
 	);
 };
 
 export default DdcUpload;
+
