a98f7f34ca2f8055bc130d247f4c7215351498a9
Prasanna Kondapaneni <prasanna@teamiconix.com>
2021-12-28 20:57:46 -0800

    added NFT attach code

--- src/Attacher.jsx
+++ src/Attacher.jsx
@@ -1,13 +1,22 @@
+import bs58 from 'bs58';
 import { useState } from "react";
 import { attach } from "./utils/attach.js";
 
 export default (_) => {
   const [tx, setTx] = useState(null);
   const [nftId, setNftId] = useState(1);
-  const [cid, setCid] = useState("1234");
+  const [cid, setCid] = useState("Qmddp2ATX5aGrvsCSaQDuXmgmH7U5JeSmYXYrJ3StYWq1n");
+  const [cidError, setCidError] = useState("");
 
   const onNftIdInput = e => setNftId(e.target.value);
-  const onCidInput = e => setCid(e.target.value);
+  const onCidInput = e => {
+  	try {
+  		const bytes = getBytes32FromIpfsHash(e.target.value);
+	  	setCid(e.target.value);
+  	} catch (err) {
+  		setCidError(err.toString());
+  	}
+  };
   const submitAttach = async () => {
     const tx = await attach(nftId, cid);
     setTx(tx.hash);
@@ -22,6 +31,7 @@ export default (_) => {
       <div>
         <span> Content ID: </span>
         <input placeholder="Content ID" value={cid} onChange={onCidInput}/>
+        <span style={{color:"red"}}>{cidError}</span>
       </div>
       <button onClick={submitAttach}> Attach </button>
       { tx ? <TxLink tx={tx}/> : ""}
@@ -36,4 +46,6 @@ const TxLink = ({tx}) => (
     Transaction Link
   </a>
 );
+const getBytes32FromIpfsHash = (ipfsListing) =>
+   "0x"+bs58.decode(ipfsListing).slice(2).toString('hex');
 
--- src/utils/attach.js
+++ src/utils/attach.js
@@ -1,4 +1,5 @@
-// import Web3 from "web3";
+import bs58 from 'bs58';
+
 import {
     importProvider,
     getNFTAttachmentAddress,
@@ -12,10 +13,7 @@ import {
 // Assumes Metamask or some other web3 wallet extension
 // Assumes browser environment
 export const attach = async (nftId, cid) => {
-	debugger
-    // e.g. "ethereum" object Metamask
     const provider = importProvider();
-	// const web3 = new Web3(provider);
     // env is one of: "stage" or "prod"
     const env = "prod"; // or stage or dev. prod is default
     // Pick smart contract address based on the environment
@@ -28,10 +26,11 @@ export const attach = async (nftId, cid) => {
     const tx = await cereFreeport.attachToNFT(
         nftId,
         // web3.fromAscii(cid)
-        cid
+        getBytes32FromIpfsHash(cid)
     );
 
     return tx;
 };
 
-
+const getBytes32FromIpfsHash = (ipfsListing) =>
+   "0x"+bs58.decode(ipfsListing).slice(2).toString('hex');
